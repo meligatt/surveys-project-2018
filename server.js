20542151035220a58906d7bb37c9f05c81e5473f
melissa gattoni <meligatt@gmail.com>
2018-08-06 10:15:37 +1000

    Add express Routes and Mock data

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
+const data = require('./data/index.json');
+
 const app = express();
 
-app.get('/api/ping', (req, res) => {
-  res.json({
-    it: "works"
-  })
-});
+app.get('/api/surveys',(req, res) => {
+  res.json(data);
+})
+
+app.get('/api/survey_results/:id',(req, res) => {
+  const id = req.params.id;
+  const survey_results = `./data/survey_results/${id}.json`;
+  res.json(require(survey_results));
+})
 
 app.listen(process.env.PORT || 8080, () => {
+  console.log("**************************");
   console.log("express runing! in 8080");
+  console.log("**************************");
 });
